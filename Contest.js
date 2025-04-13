b0224931966b8affb79873d847ab220f849da4be
SIDD1234567890 <Ranjansiddharth85@gmail.com>
2025-04-13 12:06:55 +0530

    Update Contest.js

--- Contest.js
+++ Contest.js
@@ -1,1 +1,37 @@
-// Contest.js - Dummy content
+// models/Contest.js
+
+const mongoose = require('mongoose');
+
+const contestSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  entryFee: {
+    type: Number,
+    required: true,
+  },
+  prize: {
+    type: Number,
+    required: true,
+  },
+  matchId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Match',
+    required: true,
+  },
+  totalSpots: {
+    type: Number,
+    required: true,
+  },
+  spotsFilled: {
+    type: Number,
+    default: 0,
+  },
+  users: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+}, { timestamps: true });
+
+module.exports = mongoose.model('Contest', contestSchema);// Contest.js - Dummy content
