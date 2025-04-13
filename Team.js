1c45afc3ec344e5ba1d05a41f2b7716cb95435c8
SIDD1234567890 <Ranjansiddharth85@gmail.com>
2025-04-13 12:17:01 +0530

    Update Team.js

--- Team.js
+++ Team.js
@@ -1,1 +1,37 @@
-// Team.js - Dummy content
+// models/Team.js
+
+const mongoose = require('mongoose');
+
+const teamSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  matchId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Match',
+    required: true
+  },
+  players: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Player',
+    required: true
+  }],
+  captain: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Player',
+    required: true
+  },
+  viceCaptain: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Player',
+    required: true
+  },
+  totalPoints: {
+    type: Number,
+    default: 0
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Team', teamSchema);
