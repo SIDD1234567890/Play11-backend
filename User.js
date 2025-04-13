685ced85df8e3b360f454e94e2f7b1d983bbce41
SIDD1234567890 <Ranjansiddharth85@gmail.com>
2025-04-13 12:18:04 +0530

    Update User.js

--- User.js
+++ User.js
@@ -1,1 +1,36 @@
-// User.js - Dummy content
+// models/User.js
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  walletBalance: {
+    type: Number,
+    default: 0
+  },
+  totalWinnings: {
+    type: Number,
+    default: 0
+  },
+  joinedContests: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Contest'
+  }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);// User.js - Dummy content
