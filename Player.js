029e7bab2c999f2a968715849d3841cb646b1870
SIDD1234567890 <Ranjansiddharth85@gmail.com>
2025-04-13 12:13:22 +0530

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,1 +1,34 @@
-// Player.js - Dummy content
+// models/Player.js
+
+const mongoose = require('mongoose');
+
+const playerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['batsman', 'bowler', 'all-rounder', 'wicket-keeper'],
+    required: true
+  },
+  team: {
+    type: String,
+    required: true
+  },
+  matchId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Match',
+    required: true
+  },
+  points: {
+    type: Number,
+    default: 0
+  },
+  isPlaying: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Player', playerSchema);
