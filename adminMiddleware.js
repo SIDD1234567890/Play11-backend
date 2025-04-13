56873a39eec9eea95aa602332b0677ddc77793bf
SIDD1234567890 <Ranjansiddharth85@gmail.com>
2025-04-13 13:55:04 +0530

    Create adminMiddleware.js

---
+++ adminMiddleware.js
@@ -0,0 +1,17 @@
+const User = require('../models/User');
+
+const adminMiddleware = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    if (!user || user.role !== 'admin') {
+      return res.status(403).json({ message: 'Access denied. Admins only.' });
+    }
+
+    next();
+  } catch (error) {
+    res.status(500).json({ message: 'Admin check failed', error });
+  }
+};
+
+module.exports = adminMiddleware;
